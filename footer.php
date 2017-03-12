b023fe88470028d14dd8ea9f5664d9e190ebba78
ido barnea <barbareshet@gmail.com>
2017-03-12 22:04:13 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package barbareshet
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'barbareshet' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'barbareshet' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'barbareshet' ), 'barbareshet', '<a href="https://automattic.com/" rel="designer">Ido Barnea</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
